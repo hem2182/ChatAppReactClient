4b2708991bb9ea018a4ccfc3dabf9b84b49c6696
hem2182 <hemant_2182_fresco@yahoo.com>
2020-04-18 21:43:43 +0530

    Changed endpoint to point the server url on heroku.

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -17,7 +17,8 @@ const Chat = ({ location }) => {
   const [users, setUsers] = useState('');
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
-  const ENDPOINT = "localhost:5000";
+  //const ENDPOINT = "localhost:5000";
+  const ENDPOINT = "https://react-chat-messaging-app.herokuapp.com/";
 
   // useEffect hook lets you use lifecycle methods or side effects in function components.
   // it is similar to componentDidMount and componentDidUpdate
